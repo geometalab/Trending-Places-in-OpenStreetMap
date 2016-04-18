a6e6e9dfb70da6736d1892f0ec23a72534b8a369
BhavyaLight <bhavyachandra2107@gmail.com>
2016-04-18 14:06:38 +0200

    Script to find trending places (with a 2 day lag)

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Generates the Top 10 trending places on OSM (with a 2 day lag)
+
+tile_log_diff="2"
+date_diff="7"
+
+date_to=$(date "--date=${dataset_date} -${tile_log_diff} day" +%Y-%m-%d)
+date_from=$(date "--date=${dataset_date} -${date_diff} day-1 day" +%Y-%m-%d)
+
+python3 fetch2.py --date_from=$date_from --date_to=$date_to >Trends.csv
+cat Trends.csv|python3 bubble.py --date_precision=1d --min_zoom=10 --max_zoom=19 --min_subz=10 --max_subz=10 --no_per_day>Zoom10Tiles.csv
+cat Zoom10Tiles.csv | python Top_trending.py --graph
+python Trending_Bot.py
