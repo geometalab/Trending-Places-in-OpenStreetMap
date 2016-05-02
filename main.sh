9900b401114dc3d4d1ffa86ec80b84061a3f1c1e
BhavyaLight <bhavyachandra2107@gmail.com>
2016-05-02 16:54:10 +0200

    Improving file naming conventions

--- main.sh
+++ main.sh
@@ -7,8 +7,7 @@ date_diff="7"
 date_to=$(date "--date=${dataset_date} -${tile_log_diff} day" +%Y-%m-%d)
 date_from=$(date "--date=${dataset_date} -${date_diff} day-1 day" +%Y-%m-%d)
 
-python3 fetch2.py --date_from=$date_from --date_to=$date_to >Trends.csv
-cat Trends.csv|python3 bubble.py --date_precision=1d --min_zoom=10 --max_zoom=19 --min_subz=10 --max_subz=10 --no_per_day>Zoom10Tiles.csv
-cat Zoom10Tiles.csv | python3 Top_trending.py --graph
+python3 Fetch2.py --date_from=$date_from --date_to=$date_to >Trends.csv &&
+cat Trends.csv|python3 Bubble.py --date_precision=1d --min_zoom=10 --max_zoom=19 --min_subz=10 --max_subz=10 --no_per_day>Zoom10Tiles.csv &&
+cat Zoom10Tiles.csv | python3 Top_Trending.py --graph &&
 python3 Trending_Bot.py
-
