d1146f220255e46efc52d7f510fa28e83fe68fa6
BhavyaLight <bhavyachandra2107@gmail.com>
2016-05-23 17:29:34 +0200

    Continue tweeting even when logs are unavailable

--- main.sh
+++ main.sh
@@ -9,5 +9,5 @@ date_from=$(date "--date=${date_to} -${date_diff} day+1 day" +%Y-%m-%d)
 
 python3 Fetch2.py --date_from=$date_from --date_to=$date_to >Trends.csv &&
 cat Trends.csv|python3 Bubble.py --date_precision=1d --min_zoom=14 --max_zoom=19 --min_subz=14 --max_subz=14 --no_per_day>Zoom10Tiles.csv &&
-cat Zoom10Tiles.csv | python3 Top_Trending.py --graph &&
+cat Zoom10Tiles.csv | python3 Top_Trending.py --graph
 python3 Trending_Bot.py
