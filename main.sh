e56ea4c73dff108cd333b51a51d65107dbf53a0c
BhavyaLight <bhavyachandra2107@gmail.com>
2016-05-24 10:12:26 +0200

    Changing aggregation level to improve time

--- main.sh
+++ main.sh
@@ -8,6 +8,6 @@ date_to=$(date "--date=${dataset_date} -${tile_log_diff} day" +%Y-%m-%d)
 date_from=$(date "--date=${date_to} -${date_diff} day+1 day" +%Y-%m-%d)
 
 python3 Fetch2.py --date_from=$date_from --date_to=$date_to >Trends.csv &&
-cat Trends.csv|python3 Bubble.py --date_precision=1d --min_zoom=14 --max_zoom=19 --min_subz=14 --max_subz=14 --no_per_day>Zoom10Tiles.csv &&
+cat Trends.csv|python3 Bubble.py --date_precision=1d --min_zoom=10 --max_zoom=19 --min_subz=10 --max_subz=10 --no_per_day>Zoom10Tiles.csv &&
 cat Zoom10Tiles.csv | python3 Top_Trending.py --graph
 python3 Trending_Bot.py
