2d1484d371f7b121dde6329d1bb5e2c97d14596f
BhavyaLight <bhavyachandra2107@gmail.com>
2016-05-26 17:33:12 +0200

    Adding date to Top_trending to make it more dynamic

--- main.sh
+++ main.sh
@@ -9,5 +9,5 @@ date_from=$(date "--date=${date_to} -${date_diff} day+1 day" +%Y-%m-%d)
 
 python3 Fetch2.py --date_from=$date_from --date_to=$date_to >Trends.csv &&
 cat Trends.csv|python3 Bubble.py --date_precision=1d --min_zoom=10 --max_zoom=19 --min_subz=10 --max_subz=10 --no_per_day>Zoom10Tiles.csv &&
-cat Zoom10Tiles.csv | python3 Top_Trending.py --graph
+cat Zoom10Tiles.csv | python3 Top_Trending.py --graph --date=$date_to
 python3 Trending_Bot.py
